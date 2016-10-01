e796a87e7c6d51a57251d178dd7bd566b18bf5ad
libertylocked <unlock.liberty@gmail.com>
2016-10-01 02:38:52 -0500

    Add Echo plugin

---
+++ WebScriptHook.Framework/BuiltinPlugins/Echo.cs
@@ -0,0 +1,18 @@
+﻿namespace WebScriptHook.Framework.BuiltinPlugins
+{
+    class Echo : Plugin
+    {
+        protected internal override string PluginIDImpl
+        {
+            get
+            {
+                return "echo";
+            }
+        }
+
+        public override object Respond(object[] args)
+        {
+            return args;
+        }
+    }
+}
